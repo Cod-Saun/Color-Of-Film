391e85390f12e590bda9065dbca9baa0d0c770fe
Cod-saun <csaunders8@mail.csuchico.edu>
2020-07-14 16:23:53 -0700

    Colors will now be drawn in equal size rectangles for the width of the final image

--- ColorData.py
+++ ColorData.py
@@ -1,7 +1,7 @@
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
 
-#add as command line argument
+#Add as command line argument
 path = "/mnt/c/Users/Cody/Pictures/Wallpapers/"
 filenum = 0
 final_vals = []
@@ -35,13 +35,28 @@ for file in os.listdir(path):
     print(r_avg)
     print(g_avg)
     print(b_avg)
-    
-    out = Image.new(mode="RGB",size=(100,100),color=(r_avg,g_avg,b_avg))
-    filename = "/mnt/c/Users/Cody/Pictures/Outfiles/out%d.jpg" %filenum
-    out = out.save(filename)
     filenum += 1
 
-#add as command line arguments
-print(final_vals)
-outfile_width = 500
-outfile_height = 1000
+#Add as command line arguments
+outfile_width = 1000
+outfile_height = 500
+color_width = outfile_width/len(final_vals)
+#Coordinates for color rectangles to be drawn
+cord1 = (0, 0)
+cord2 = (color_width, 500)
+
+#create new image for color rectangles to be drawn
+img = Image.new(mode="RGB",size=(outfile_width, outfile_height))
+draw = ImageDraw.Draw(img)
+#Iterate through each color in the final color values array and draw a rectangle for that color
+#Each rectangle is a given width to fill the width of the image
+#Trying to find more efficient code than converting frol tuples to lists back to tuples
+for i in range(0, len(final_vals)):
+    draw.rectangle([cord1, cord2], fill=final_vals[i])
+    cord1 = list(cord1)
+    cord1[0] += color_width
+    cord1 = tuple(cord1)
+    cord2 = list(cord2)
+    cord2[0] += color_width
+    cord2 = tuple(cord2)
+img = img.save("/mnt/c/Users/Cody/Pictures/testout.jpeg")
