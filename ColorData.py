bac23461369c04c7b853d5d8a77998eff37bf438
Cod-saun <csaunders8@mail.csuchico.edu>
2020-08-13 20:19:44 -0700

    added command line arguments for paths

--- ColorData.py
+++ ColorData.py
@@ -1,9 +1,19 @@
 import os
+import sys
 import numpy
 from PIL import Image, ImageDraw
 
-#Add as command line argument
-path = "/mnt/c/Users/Cody/Pictures/Wallpapers/"
+if len(sys.argv) < 5:
+    print("Too few arguments recieved: must provide path to images directory and final image")
+    print("python Colordata.py [path to images] [path to output file] [width of final image] [height of final image]")
+    sys.exit()
+if len(sys.argv) > 5:
+    print("Too many arguments recieved.")
+    print("python Colordata.py [path to images] [path to output file]")
+    sys.exit()
+
+path = sys.argv[1]
+finalpath = sys.argv[2]
 filenum = 1
 final_vals = []
 for file in os.listdir(path):
@@ -15,21 +25,19 @@ for file in os.listdir(path):
     rgb_avg = rgb_avg.astype(int)
     final_vals.append(tuple(rgb_avg))
     print(rgb_avg)
-    print("file #" + str(filenum) + " of 38")
+    print("file #" + str(filenum))
     filenum += 1 
 
-print(final_vals)
-
 #Add as command line arguments 
-outfile_width = 1000
-outfile_height = 500
-color_width = outfile_width/len(final_vals)
+outfile_width = sys.argv[3]
+outfile_height = sys.argv[4]
+color_width = int(outfile_width)/len(final_vals)
 #Coordinates for color rectangles to be drawn
 cord1 = (0, 0)
-cord2 = (color_width, 500)
+cord2 = (color_width, int(outfile_height))
 
 #create new image for color rectangles to be drawn
-img = Image.new(mode="RGB",size=(outfile_width, outfile_height))
+img = Image.new(mode="RGB",size=(int(outfile_width), int(outfile_height)))
 draw = ImageDraw.Draw(img)
 #Iterate through each color in the final color values array and draw a rectangle for that color
 #Each rectangle is a given width to fill the width of the image
@@ -42,4 +50,4 @@ for i in range(0, len(final_vals)):
     cord2 = list(cord2)
     cord2[0] += color_width
     cord2 = tuple(cord2)
-img = img.save("/mnt/c/Users/Cody/Pictures/testout2.png")
+img = img.save(finalpath)
